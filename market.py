cc7da1002ecd44cece0e478ba28f0e1f0d4b263f
RndMnkIII <rndmnkiii@gmail.com>
2021-05-18 16:00:59 +0000

    Adding Bootstrap and personal CSS styles

--- market.py
+++ market.py
@@ -1,16 +1,18 @@
 # market.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
 
 #Dynamic route
 @app.route('/about/<username>')
 def about(username):
     return f'<h1>This is the about page of {username}</h1>'
 
-
+if __name__ == "__main__":
+    app.run(host='rndmnkiii.com', port='4000', debug=True)
 
