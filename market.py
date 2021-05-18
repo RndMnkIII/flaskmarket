2c3e9e1d155a75cbf1a2088237804b45a2cf2409
RndMnkIII <rndmnkiii@gmail.com>
2021-05-18 11:50:58 +0000

    Adding routes

--- market.py
+++ market.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/about/<username>')
+def about(username):
+    return f'<h1>This is the about page of {username}</h1>'
+
+
+
