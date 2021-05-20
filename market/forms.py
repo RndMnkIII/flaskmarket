68a4d97f8c2e132a80f63d55524558aa51c68bac
RndMnkIII <rndmnkiii@gmail.com>
2021-05-20 11:00:28 +0000

    Flash messages and advanced validation

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exist! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        user = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if user:
+            raise ValidationError('User with this email address already exist! Please try a different email address')
+
+
+
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()] )
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()] )
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()] )
